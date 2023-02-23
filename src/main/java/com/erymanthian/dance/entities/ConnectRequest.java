7f9a2902463a167c44da0a87e12dcb72a668d7ef
amir <amirhali2022@gmail.com>
2023-02-23 18:04:33 +0330

    resolve bug

--- src/main/java/com/erymanthian/dance/entities/ConnectRequest.java
+++ src/main/java/com/erymanthian/dance/entities/ConnectRequest.java
@@ -4,10 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Transient;
+import org.springframework.core.io.Resource;
 
 @Entity
 @NoArgsConstructor
@@ -26,7 +27,43 @@ public class ConnectRequest {
     private Short status;
 
     @Transient
-    private Boolean isForMe;
+    private String companyName;
+
+    @Transient
+    private String dancerName;
+
+    @Transient
+    private String eventName;
+
+    @Transient
+    private String image;
+
+    @Transient
+    private String lastname;
+
+    @Transient
+    private String resource;
+
+    public ConnectRequest(Long id, Integer eventId, Long sourceDancer, Short status,
+                          String dancerName, String eventName, String lastname, String image) {
+        this.id = id;
+        this.eventId = eventId;
+        this.sourceDancer = sourceDancer;
+        this.status = status;
+        this.dancerName = dancerName;
+        this.eventName = eventName;
+        this.lastname = lastname;
+        this.image = image;
+    }
+
+    public ConnectRequest(Long id, Integer eventId, Long sourceDancer, Short status, String companyName, String eventName) {
+        this.id = id;
+        this.eventId = eventId;
+        this.sourceDancer = sourceDancer;
+        this.status = status;
+        this.companyName = companyName;
+        this.eventName = eventName;
+    }
 
     public ConnectRequest(Integer eventId, Long sourceDancer, Short status) {
         this.eventId = eventId;
